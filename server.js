08fd82a743f5ff32ba9282ea8e2b3646db145468
Sanjay Saha <sanjaysaha1120@gmail.com>
2023-09-19 12:10:12 +0530

    remove errors

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const {chats} = require("./data/data");
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
@@ -38,8 +36,7 @@ app.get('/',(req,res)=>{
 });
 
 app.use('/api/user',userRoutes);
-app.use('/api/chat',chatRoutes);
-app.use('/api/message',messageRoutes)
+
 
 
 
